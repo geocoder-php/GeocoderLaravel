090bc89a3771b8c456115cf476968b6cfd96cea4
Mike Bronner <mike@genealabs.com>
2016-10-13 20:02:33 -0700

    Place namespace declaration below file comment

--- src/Exceptions/InvalidDumperException.php
+++ src/Exceptions/InvalidDumperException.php
@@ -1,13 +1,15 @@
-<?php namespace Geocoder\Laravel\Exceptions;
+<?php
 
 /**
  * This file is part of the GeocoderLaravel library.
  *
- * (c) Antoine Corcy <contact@sbin.dk>
+ * (c) Mike Bronner <hello@genealabs.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
+namespace Geocoder\Laravel\Exceptions;
 
 use Geocoder\Exception\Exception;
 use Exception as BaseException;
