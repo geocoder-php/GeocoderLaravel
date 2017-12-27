2cebf12c554d470520a4fd74bd2ce96ce540fddb
Mike Bronner <mike@genealabs.com>
2017-12-27 13:00:33 -0800

    Update config to read locale from .env, add descriptions

--- config/geocoder.php
+++ config/geocoder.php
@@ -1,26 +1,67 @@
 <?php
 
-/**
- * This file is part of the GeocoderLaravel library.
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Geocoder\Provider\Chain\Chain;
 use Geocoder\Provider\GeoPlugin\GeoPlugin;
 use Geocoder\Provider\GoogleMaps\GoogleMaps;
 use Http\Client\Curl\Client;
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Duration
+    |--------------------------------------------------------------------------
+    |
+    | Specify the cache duration in seconds. The default approximates a forever
+    | cache, but there are certain issues with Laravel's forever caching
+    | methods that prevent us from using them in this project.
+    |
+    | Default: 9999999 (integer)
+    |
+    */
     'cache-duration' => 9999999,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Providers
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify any number of providers that should be used to
+    | perform geocaching operations. The `chain` provider is special,
+    | in that it can contain multiple providers that will be run in
+    | the sequence listed, should the previous provider fail. By
+    | default the first provider listed will be used, but you
+    | can explicitly call subsequently listed providers by
+    | alias: `app('geocoder')->using('google_maps')`.
+    |
+    | Please consult the official Geocoder documentation for more info.
+    | https://github.com/geocoder-php/Geocoder#providers
+    |
+    */
     'providers' => [
         Chain::class => [
             GoogleMaps::class => [
-                'en-US',
+                env('GOOGLE_MAPS_LOCALE', 'en-US'),
                 env('GOOGLE_MAPS_API_KEY'),
             ],
             GeoPlugin::class  => [],
         ],
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Adapter
+    |--------------------------------------------------------------------------
+    |
+    | You can specify which PSR-7-compliant HTTP adapter you would like to use.
+    | There are multiple options at your disposal: CURL, Guzzle, and others.
+    |
+    | Please consult the official Geocoder documentation for more info.
+    | https://github.com/geocoder-php/Geocoder#usage
+    |
+    | Default: Client::class (FQCN for CURL adapter)
+    |
+    */
     'adapter'  => Client::class,
+
 ];
